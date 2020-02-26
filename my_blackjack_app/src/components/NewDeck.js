aed0d550049a7888d13bfd8083f18dda5ac46844
Wilsantos1975 <faustosantos@pursuit.org>
2020-02-25 20:16:29 -0500

    display deck if using componentDidMount

--- my_blackjack_app/src/components/NewDeck.js
+++ my_blackjack_app/src/components/NewDeck.js
@@ -1,35 +1,42 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class NewDeck extends React.Component {
-    State = { deck: [] };
+  state = { deck: "" };
 
-        freshDeck = async () => {
-            try {
-                let res = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-                let deckId = res.data.deck_id
-                debugger
-                let drawTwoCards = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-                // debugger
-                this.setState({ deck: res })
-            
-            } catch (error) {
-                this.setState({deck: []})
-                console.log(error)
+//   componentDidMount(){
+// this.freshDeck()
+//   }
 
-            }
-        }
-        render() {
-            return (
-                <section>
+  freshDeck = async () => {
+    try {
+      let res = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      let  deck = res.data;
+      let deckId = deck.deck_id;
+      this.setState({ deck: deckId })
+      debugger;
+    
 
-                    <button onClick={this.freshDeck}>hit me</button>
-                </section>
-                
-            )
-            
-        }
+    //   let drawTwoCards = await axios.get(
+    //     `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+    //   );
 
+    //   this.setState({ deck: res });
+    } catch (error) {
+    //   this.setState({ deck: [] });
+      console.log(error);
+    }
+  };
+  render() {
+    return (
+      <section>
+        <button onClick={this.freshDeck}>start game</button>
+        <p>Deck ID :{this.state.deck}</p>
+      </section>
+    );
+  }
 }
 
 export default NewDeck;
